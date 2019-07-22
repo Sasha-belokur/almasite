70f26c7fa5eebdc25453173ae48fe06c3fc845b3
Alex Belokur <alex.belokur17@gmail.com>
2019-07-22 21:49:23 +0300

    nice

---
+++ precache-manifest.35e0bdcd28cd98a4cea135f3c891203b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae20bb33fadeef1b913f3f13423eae7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d65a3bcebdc10ce7a700",
+    "url": "/static/css/main.a9298055.chunk.css"
+  },
+  {
+    "revision": "b90a4b3bbb39863457f2",
+    "url": "/static/js/2.ac9a9419.chunk.js"
+  },
+  {
+    "revision": "d65a3bcebdc10ce7a700",
+    "url": "/static/js/main.f6cde39a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "360a86f170a97d205bbbe4f115392218",
+    "url": "/static/media/background-desktop.360a86f1.png"
+  },
+  {
+    "revision": "6bc4b1bd4ddfd5b3bad4949b63499786",
+    "url": "/static/media/background-tablet.6bc4b1bd.png"
+  },
+  {
+    "revision": "0207b5e6cd42a4e6f0ac33b16bc0c2e8",
+    "url": "/static/media/cyrvetica.0207b5e6.otf"
+  },
+  {
+    "revision": "0d98a7b61fc4a18693c173b867cb7263",
+    "url": "/static/media/logo-desktop.0d98a7b6.png"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/static/media/monsterrat-regular-latin.bc3aa95d.woff2"
+  },
+  {
+    "revision": "bfdfc1b030afe28ea4e0eb485b93b42c",
+    "url": "/static/media/montserrat-extrabold-cyrillic.bfdfc1b0.woff2"
+  },
+  {
+    "revision": "b7018be9ed6cd94da8b6675b3a468c3b",
+    "url": "/static/media/montserrat-extrabold-latin.b7018be9.woff2"
+  },
+  {
+    "revision": "a4aa91fad2108f37ed4721c2221efc24",
+    "url": "/static/media/montserrat-medium-cyrillic.a4aa91fa.woff2"
+  },
+  {
+    "revision": "92d16e458625f4d2c8940f6bdca0ff09",
+    "url": "/static/media/montserrat-medium-latin.92d16e45.woff2"
+  },
+  {
+    "revision": "298b48e6dfdf7ec4ef8907ff8a7182b1",
+    "url": "/static/media/montserrat-regular-cyrillic.298b48e6.woff2"
+  },
+  {
+    "revision": "67b4a56c5d3c35db36c3c7482886f7f2",
+    "url": "/static/media/montserrat-semibold-cyrillic.67b4a56c.woff2"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/static/media/montserrat-semibold-latin.6fb1b562.woff2"
+  }
+]);
